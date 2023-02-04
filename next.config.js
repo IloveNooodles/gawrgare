8659638372cd2de71b945cbebca9955cdee5397b
IloveNooodles <mgarebaldhie80@gmail.com>
2023-02-05 01:01:20 +0700

    feat: adding markdown and move into scss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require("@next/mdx")({
+  // Optionally provide remark and rehype plugins
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+});
+
 const nextConfig = {
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   experimental: {
     appDir: true,
+    mdxRs: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig);
