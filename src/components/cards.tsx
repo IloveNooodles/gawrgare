f3ea3732b3b45b34cf6aba4dd93396be0d50247c
IloveNooodles <mgarebaldhie80@gmail.com>
2023-06-10 02:31:15 +0700

    feat: projects half complete

---
+++ src/components/cards.tsx
@@ -0,0 +1,40 @@
+import styles from '@/styles/cards.module.scss';
+import { SiFirefoxbrowser, SiGithub } from 'react-icons/si';
+
+export interface CardProps {
+  title: string;
+  description: string;
+  githubLink: string;
+  websiteLink: string;
+  techStackUsed: string;
+  category: string;
+  date: string
+}
+
+export default function Cards(props: CardProps) {
+  const { title, description, githubLink, websiteLink, techStackUsed } = props;
+
+  return (
+    <article className={styles.cards}>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>
+          Techstack: <span>{techStackUsed}</span>
+        </p>
+        <div className={styles['cards-links']}>
+          <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            <SiGithub />
+            <p>Github</p>
+          </a>
+          {websiteLink && (
+            <a href={websiteLink} target="_blank" rel="noopener noreferrer">
+              <SiFirefoxbrowser />
+              <p>Website</p>
+            </a>
+          )}
+        </div>
+      </div>
+    </article>
+  );
+}
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,16 +1,39 @@
+import Cards, { CardProps } from '@/components/cards';
 import Seo from '@/components/seo';
-import styles from "@/styles/projects.module.scss"
 import { ubuntuMono } from '@/fonts/fonts';
+import styles from '@/styles/projects.module.scss';
+
+const PROJECT_LIST: CardProps[] = [
+  {
+    title: 'DNAObama',
+    description: 'Dna string pattern matching website',
+    techStackUsed: 'React, Sequelize, Express, CSS',
+    githubLink: 'https://github.com/IloveNooodles/dna-obama',
+    websiteLink: 'https://dna-obama.vercel.app/',
+  },
+  {
+    title: 'DNAObama',
+    description: 'Dna string pattern matching website',
+    techStackUsed: 'React, Sequelize, Express, CSS',
+    githubLink: 'https://github.com/IloveNooodles/dna-obama',
+    websiteLink: 'https://dna-obama.vercel.app/',
+  },
+];
 
 export default function Projects() {
   return (
     <>
       <Seo title="Muhammad Garebaldhie - Projects" />
-      <main className={`${ubuntuMono.className}`}>
+      <main className={`${ubuntuMono.className} ${styles.projects}`}>
         <h1>
           {' '}
           me@gawrgare:~$ <span>cat projects</span>
         </h1>
+        <div className={styles['project-container']}>
+          {PROJECT_LIST.map((project, index) => {
+            return <Cards {...project} key={index} />;
+          })}
+        </div>
       </main>
     </>
   );
